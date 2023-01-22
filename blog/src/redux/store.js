71f6e0ac0b665777648c2623052b938a78d6433d
kawsar firoz <90864901+kawsar334@users.noreply.github.com>
2023-01-22 08:14:31 -0800

    Update store.js

--- blog/src/redux/store.js
+++ blog/src/redux/store.js
@@ -1,19 +1,14 @@
 import { configureStore , combineReducers} from '@reduxjs/toolkit'
 import userSlice from './useSlice';
 import { persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-
-
-
-
-
+import storage from 'redux-persist/lib/storage';
 const persistConfig = {
     key:"root",
     version:1,
     storage,
 
 }
-const rootReducer = combineReducers({ user: userSlice })
+const rootReducer = combineReducers({ user: userSlice });
 
 const persistReduce = persistReducer(persistConfig,rootReducer )
 export const store = configureStore({
@@ -22,6 +17,5 @@ export const store = configureStore({
         ignoredActions:[FLUSH, REGISTER, REHYDRATE, PURGE, PERSIST,PAUSE]
     }
 });
-
 
 export const persistStor=persistStore(store)
